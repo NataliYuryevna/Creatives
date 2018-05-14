526ea95289425af3b04488403406f1221631caac
NataliYuryevna <33745739+NataliYuryevna@users.noreply.github.com>
2018-05-14 16:11:06 +0300

    исключить bootstrap, адаптивность сайта

---
+++ www/js/header.js
@@ -0,0 +1,31 @@
+/*var bars = document.getElementsByClassName('flaticon-bars');
+if(bars.length){
+	bars[0].onclick = function(){
+		onclickBar(this);
+	}
+}
+
+if (window.matchMedia('(min-width: 1058px)').matches){
+	console.log("djn");
+	var nav = document.getElementsByTagName('nav');
+	if(nav[0].classList.contains('open')){
+		nav[0].classList.remove('open');
+	}
+	else if(nav[0].classList.contains('close')){
+		nav[0].classList.remove('close');
+	}
+}
+
+function onclickBar(bar){
+	var nav = document.getElementsByTagName('nav');
+	if(nav[0].classList.contains('open')){
+		nav[0].classList.add('close');
+		nav[0].classList.remove('open');
+	}
+	else{
+		nav[0].classList.remove('close');
+		nav[0].classList.add('open');
+	}
+}*/
+
+
